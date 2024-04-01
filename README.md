f2e712d8e88ee59988c3aa15b040e811457da1b5
Christopher GERARD <ger.chr51@gmail.com>
2024-04-01 15:45:36 +0200

    Ajout d'une validation sur le paramètre MaxGear, complétion de la documentation intégtée et écriture du fichier README.md
    Signed-off-by: Christopher GERARD <ger.chr51@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,91 @@
 # Get-RandomGear
 
----
-#### Script permettant de récupérer une vitesse aléatoirement et de choisir entre une rétrogadation ou un passage de vitesse.
----
+Script PowerShell permettant de récupérer une vitesse aléatoirement et de choisir entre une rétrogradation ou un passage de vitesse.
+
+Par défaut, le choix de la vitesse se fait entre 1 et 6.
+
+## Syntaxe
+
+```POWERSHELL
+.\Get-RandomGear.ps1
+    [[-MaxGear] <int>]
+    [<CommonParameters>]
+```
+
+## Paramètre
+
+__-MaxGear__
+
+Spécifie une valeur entière strictement supérieure à 1 précisant le choix de la vitesse maximale souhaitée.
+
+La valeur par défaut de ce paramètre est 6.
+
+Ce paramètre n'est pas obligatoire.
+ 
+## Exemple 1
+
+### Commande:
+```POWERSHELL
+.\Get-RandomGear.ps1
+```
+
+### Résultat:
+
+Le script retourne une vitesse et une action.
+
+```POWERSHELL
+Gear GearBoxAction
+---- -------------
+   4 Down
+```
+## Exemple 2
+
+### Commande:
+```POWERSHELL
+.\Get-RandomGear.ps1 -MaxGear 5
+```
+
+### Résultat:
+
+Le script retourne une vitesse entre 1 et 5 et une action.
+
+```POWERSHELL
+Gear GearBoxAction
+---- -------------
+   1 Up
+```
+
+## Exemple 3
+
+### Commande:
+```POWERSHELL
+for ( ($i = 0); $i -lt 10; $i++ ){
+    .\Get-RandomGear.ps1
+    Start-Sleep -Seconds 1
+}
+```
+
+### Résultat:
+
+Le script retourne une vitesse et une action 10 fois avec une pause de 1 seconde entre chaque retour.
+
+```POWERSHELL
+Gear GearBoxAction
+---- -------------
+   2 Up
+   3 Up
+   2 Down
+   5 Down
+   2 Down
+   5 Up
+   1 Up
+   6 Down
+   6 Down
+   6 Down
+```
+
+## Note
+
+Ce script a été conçu dans l'objectif de s'entraîner, avant une séance de conduite, au bon placement de la main lors des passages de vitesse et des rétrogradations.
+
+Une fois que le script retourne une vitesse et une action, l'idée est de placer sa main instinctivement de la façon la plus optimale pour effectuer l'action en question.
--- src/Get-RandomGear.ps1
+++ src/Get-RandomGear.ps1
@@ -34,24 +34,76 @@
 
 <# 
 
+.SYNOPSIS
+Script permettant de récupérer une vitesse aléatoirement et de choisir entre une rétrogradation ou un passage de vitesse.
+
 .DESCRIPTION 
- Script permettant de récupérer une vitesse aléatoirement et de choisir entre une rétrogradation ou un passage de vitesse 
+Script permettant de récupérer une vitesse aléatoirement et de choisir entre une rétrogradation ou un passage de vitesse.
+
+.PARAMETER MaxGear
+Spécifie une valeur entière strictement supérieure à 1 précisant le choix de la vitesse maximale souhaitée.
+
+.EXAMPLE
+PS> .\Get-RandomGear.ps1
+
+Gear GearBoxAction
+---- -------------
+   4 Down
+
+Cette commande retourne une vitesse comprise entre 1 et 6 et une action qui peut être soit un passage de vitesse, soit une rétrogradation.
+
+.EXAMPLE
+PS> .\Get-RandomGear.ps1 -MaxGear 5
+
+Gear GearBoxAction
+---- -------------
+   1 Up
+
+Cette commande retourne une vitesse comprise entre 1 et 5 (si un véhicule ne dispose que de 5 vitesses par exemple) et une action qui peut être soit un passage de vitesse, soit une rétrogradation.
+
+.EXAMPLE
+PS> for ( ($i = 0); $i -lt 10; $i++ ){
+    .\Get-RandomGear.ps1
+    Start-Sleep -Seconds 1
+}
+
+Gear GearBoxAction
+---- -------------
+   2 Up
+   3 Up
+   2 Down
+   5 Down
+   2 Down
+   5 Up
+   1 Up
+   6 Down
+   6 Down
+   6 Down
+
+Cette commande permet de jouer le script 10 fois avec une pause de 1 seconde entre chaque exécution. Elle permet de retourner 10 fois une vitesse comprise entre 1 et 6 et une action qui peut être soit un passage de vitesse, soit une rétrogradation.
+
+.OUTPUTS
+System.Management.Automation.PSCustomObject
+
+.NOTES
+Ce script a été conçu dans l'objectif de s'entraîner, avant une séance de conduite, au bon placement de la main lors des passages de vitesse et des rétrogradations.
+
+Une fois que le script retourne une vitesse et une action, l'idée est de placer sa main instinctivement de la façon la plus optimale pour effectuer l'action en question.
 
 #>
 [CmdletBinding()]
 Param(
     # Choix de la vitesse maximale
     [Parameter(Mandatory=$false, HelpMessage="Paramètre permettant de choisir une vitesse maximale")]
+    [ValidateScript({
+        $_ -gt 1
+    })]
     [int]
-    $MaxGear = 6,
-
-    # Vitesse de rafraichissement
-    [Parameter(Mandatory=$false, HelpMessage="Paramètre permettant de définir une vitesse de rafraichissement du script")]
-    [int]
-    $RefreshSpeed
+    $MaxGear = 6
 )
 
 $gear = Get-Random -Minimum 1 -Maximum $($MaxGear+1)
+
 if ($gear -eq $MaxGear){
     $gearBoxAction = "Down"
 }
